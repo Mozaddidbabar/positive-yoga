6a7a41e063dbcb669b2e5deb7cfa7c0fee930570
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-04-07 03:37:09 +0600

    hello

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-    "./node_modules/tw-elements/dist/js/**/*.js",
-    "./node_modules/flowbite/**/*.js",
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './node_modules/tw-elements/dist/js/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("tw-elements/dist/plugin"), require("flowbite/plugin")],
-};
+  plugins: [require('tw-elements/dist/plugin')],
+}
