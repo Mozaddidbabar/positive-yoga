3685cbfdf7b210a89e38ad3ee5f2ce99f7d53fbc
Mozaddidbabar <babar.mozaddid@gmail.com>
2022-04-07 05:05:24 +0600

    first commit

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Header from "../../../shared/Header/Header";
-import Rightness from "../../Rightness/Rightness";
-import Clients from "../Clients/Clients";
-import Discount from "../Discount/Discount";
-import FAQ from "../FAQ/FAQ";
-import MobileClient from "../MobileClient/MobileClient";
-import Plan from "../Plan/Plan";
+import React from 'react'
+import Header from '../../../shared/Header/Header'
+import Clients from '../Clients/Clients'
+import Discount from '../Discount/Discount'
+import FAQ from '../FAQ/FAQ'
+import List from '../List/List'
+import MobileClient from '../MobileClient/MobileClient'
+import Plan from '../Plan/Plan'
 
 const Home = () => {
   return (
@@ -14,27 +14,27 @@ const Home = () => {
       <Header></Header>
       <Plan>
         <div>
-          <p className="lg:text-center sm:text-left ml-4">
-            Over <span className="font-bold">52 147</span> plans ordered
+          <p className='lg:text-center sm:text-left ml-4'>
+            Over <span className='font-bold'>52 147</span> plans ordered
           </p>
-          <h1 className="lg:text-center font-bold text-5xl mb-5 sm:text-left mx-4">
+          <h1 className='lg:text-center font-bold text-5xl mb-5 sm:text-left mx-4'>
             Get access to your yoga program now!
           </h1>
         </div>
       </Plan>
       <Clients></Clients>
       <MobileClient></MobileClient>
-      <Rightness></Rightness>
+      <List></List>
       <FAQ></FAQ>
       <Plan>
         <div>
-          <h1 className="text-2xl text-center font-bold lg:text-4xl">
+          <h1 className='text-2xl text-center font-bold lg:text-4xl'>
             Start your yoga program today!
           </h1>
         </div>
       </Plan>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
